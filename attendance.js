575247edc734f1f565f22f17ead63116c2e97e29
SohaHarne <harnesoha@gmail.com>
2026-06-14 11:50:20 +0530

    Add files via upload
    Will fix the date order.

---
+++ attendance.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const attendanceSchema = new mongoose.Schema(
+  {
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+
+    date: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["Present", "Absent"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Attendance =
+  mongoose.models.Attendance ||
+  mongoose.model("Attendance", attendanceSchema);
+
+export default Attendance;
